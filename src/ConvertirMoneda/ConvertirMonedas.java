d30f60cf5e4253007efdc5ee456b7c035c0839f1
Javier Villca <javieranibalvillca@gmail.com>
2022-12-17 19:40:17 -0300

    Menu creado y logica de conversion a dolar en medio camino

--- src/ConvertirMoneda/ConvertirMonedas.java
+++ src/ConvertirMoneda/ConvertirMonedas.java
@@ -1,5 +1,8 @@
 package ConvertirMoneda;
 
 public class ConvertirMonedas {
-
+	
+	public void ConvertirPesosADolares(Double inputUsuario) {
+		double monedaConvertida = inputUsuario * 0.0058;
+	}
 }
